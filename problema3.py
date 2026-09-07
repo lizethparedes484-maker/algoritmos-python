e73f5c2c442d882d84ba7a729f087f6d430f378e
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-07 16:49:53 -0700

    Delete problema3.py

--- problema3.py
+++
@@ -1,6 +0,0 @@
-import math
-minutos_estancia = int(input("¿cuanto tiempo tiene de estancia?(minutos)"))
-precio_hora = 20
-redondeo = math.ceil (minutos_estancia/60)
-cobro = precio_hora*redondeo
-print(f"su pago es de: {cobro}")
