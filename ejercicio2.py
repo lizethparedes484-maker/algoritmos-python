4e65a29887a32e735487c26fe4df08a5656bff40
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-07 16:53:24 -0700

    Add files via upload

---
+++ ejercicio2.py
@@ -0,0 +1,4 @@
+persona_edad = int(input("ingrese su año de nacimiento"))
+año_actual= 2026
+edad = año_actual - persona_edad
+print (f"su edad es {edad}")
---
+++ ejercicio6.py
@@ -0,0 +1,4 @@
+minuto = int(input("ingrese los minutos de llamada"))
+costo_minuto = 3
+costo_llamada = minuto* costo_minuto
+print(f"su total a pagar es:, {costo_llamada}$")
