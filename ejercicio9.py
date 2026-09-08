185d622b90f691aefa0c170e83253334234ce3e3
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-08 07:44:34 -0700

    Add files via upload

---
+++ ejercicio9.py
@@ -0,0 +1,10 @@
+cal1 = float(input("ingrese la primera calificacion:"))
+cal2 = float(input("ingrese la tercera calificacion:"))
+cal3 = float(input("ingrese la tercera  calificacion:"))
+
+cal11 = cal1*0.25
+cal12= cal2 *0.25
+cal13 = cal3 * 50
+
+total = (cal1+cal2+cal3 ) / 3
+print (f"el promedio es {total}")
