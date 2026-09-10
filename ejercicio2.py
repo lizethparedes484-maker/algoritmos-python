e23c0b8eb9b836e149a1705c6619ad02f6f5a939
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:17:16 -0700

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,3 +1,5 @@
+#Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año enque nacieron.
+
 persona_edad = int(input("ingrese su año de nacimiento"))
 año_actual= 2026
 edad = año_actual - persona_edad
