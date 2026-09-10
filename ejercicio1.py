9f3d60424ce0dc48dad54d7343eb180be357e914
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:18:02 -0700

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,5 @@
+#Una empresa importadora desea determinar cuántos dólares puede adquirir con equis cantidad de dinero mexicano.
+
 dolar = 16.96
 pesos = float(input("ingrese los pesos mexicanos a convertir"))
 conversion = pesos / dolar
