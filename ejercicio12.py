84773e9ac55cfb30521b52474976b82a2aef8f46
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:06:29 -0700

    Update ejercicio12.py

--- ejercicio12.py
+++ ejercicio12.py
@@ -1,3 +1,5 @@
+#“La langosta ahumada” es una empresa dedicada a ofrecer banquetes; sus tarifas son las siguientes: el costo de platillo por persona es de $95.00, pero si el número de personas es mayor a 200 pero menor o igual a 300, el costo es de $85.00. Para más de 300 personas el costo por platillo es de $75.00. Se requiere un algoritmo que ayude a determinar el presupuesto que se debe presentar a los clientes que deseen realizar un evento.
+
 personas = int(input("¿Cuántos personas son? "))
    
 if personas > 200 and personas <=300:
