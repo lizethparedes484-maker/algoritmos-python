26198240144f19935201cc5b0ab79cbbca9de611
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:16:00 -0700

    Update ejercicio3.py

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,3 +1,5 @@
+#Un estacionamiento requiere determinar el cobro que debe aplicar a las personas que lo utilizan. Considere que el cobro es con base en las horas que lo disponen y que las fracciones de hora se toman como completas
+
 import math
 minutos_estancia = int(input("¿cuanto tiempo tiene de estancia?(minutos)"))
 precio_hora = 20
