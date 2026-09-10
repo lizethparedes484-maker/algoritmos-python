d361ebb1437766dd71470a23b5cab564ca5fac40
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:07:46 -0700

    Update ejercicio11.py

---
+++ ejercicio11.py
@@ -0,0 +1,16 @@
+#Se requiere determinar cuál de tres cantidades proporcionadas es la mayor.
+
+num1 = float(input("Ingrese el primer numero: "))
+num2 = float(input("Ingrese el segundo numero: "))
+num3 = float(input("Ingrese el tercer numero: "))
+
+
+if num1 >= num2 and num1 >= num3:
+    mayor = num1
+elif num2 >= num1 and num2 >= num3:
+    mayor = num2
+else:
+    mayor = num3
+
+
+print(f"\nLa cantidad mayor es: {mayor}")
