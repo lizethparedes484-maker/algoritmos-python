b541f24a63b07c9f6cf61869a26cb3844a20fc07
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:10:25 -0700

    Update ejercicio9.py

--- ejercicio9.py
+++ ejercicio9.py
@@ -1,3 +1,5 @@
+#Determina el promedio que obtendrá un alumno considerando que realiza tres exámenes, de los cuales el primero y el segundo tienen una ponderación de 25%, mientras que el tercero de 50%
+
 cal1 = float(input("ingrese la primera calificacion:"))
 cal2 = float(input("ingrese la tercera calificacion:"))
 cal3 = float(input("ingrese la tercera  calificacion:"))
