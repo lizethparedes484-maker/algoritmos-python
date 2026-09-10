f9cab0fb041d033551e16e9bce226c50629826b4
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:14:56 -0700

    Update ejercicio4.py

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,3 +1,7 @@
+#La compañía de autobuses “La curva loca” requiere determinar el costo que tendrá el boleto de un viaje sencillo, esto basado en los kilómetros por recorrer y en el costo por kilómetro.
+
+Costo por km: $80.00 mxn
+
 km_recorridos = int(input("¿cuantos kilometros va a recorrer?"))
 kilometro = 80
 costo_total = km_recorridos* kilometro
