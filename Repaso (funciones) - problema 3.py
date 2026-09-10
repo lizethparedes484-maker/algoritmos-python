c4002b10ffa9751c4b566599adae6d3bc6f7fcf7
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 19:20:48 -0700

    Add files via upload

---
+++ Repaso (funciones) - problema 3.py
@@ -0,0 +1,9 @@
+#Diseña una función llamada repetir_texto(texto, veces) que devuelva la palabra repetida la cantidad de veces indicada. Prueba 1: repetir_texto("Eco", 3): "EcoEcoEco"Prueba 2: repetir_texto("Hola", 1): "Hola" Prueba 3: repetir_texto("Ja", 4): "JaJaJaJa"
+def repetir_texto (texto, veces):
+    return texto*veces
+
+print (repetir_texto("darinel ",4))
+
+
+
+
---
+++ Repaso (funciones) - problema 4.py
@@ -0,0 +1,7 @@
+#Crea una función llamada obtener_doble(n) que tome un número y devuelva su valor multiplicado por 2. Prueba 1: obtener_doble(14): 28 Prueba 2: obtener_doble(0): 0 Prueba 3: obtener_doble(-5): -10
+def obtener_double (n):
+    return n*2
+
+print(obtener_double(14))
+
+
