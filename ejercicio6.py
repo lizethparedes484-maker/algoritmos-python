4f848cecefb2a94cf64a48b09093d841643e21f1
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:14:03 -0700

    Update ejercicio6.py

--- ejercicio6.py
+++ ejercicio6.py
@@ -1,3 +1,5 @@
+#Se requiere determinar el costo que tendrá realizar una llamada telefónica con base en el tiempo que dura la llamada y en el costo por minuto. costo por minuto: $3.00 mxn
+
 minuto = int(input("ingrese los minutos de llamada"))
 costo_minuto = 3
 costo_llamada = minuto* costo_minuto
