5a2b770c3e2fed0fb3966b0e54c6c15a3083f73d
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-09 18:19:13 -0700

    Update tarea1.py

--- tarea1.py
+++ tarea1.py
@@ -1,3 +1,5 @@
+#El director de una escuela está organizando un viaje de estudios, y requiere determinar cuánto debe cobrar a cada alumno y cuánto debe pagar a la compañía de viajes por el servicio. La forma de cobrar es la siguiente: si son 100 alumnos o más, el costo por cada alumno es de $65.00; de 50 a 99 alumnos, el costo es de $70.00, de 30 a 49, de $95.00, y si son menos de 30, el costo de la renta del autobús es de $4000.00, sin importar el número de alumnos.
+
 alumnos = int(input("¿Cuántos alumnos van al viaje? "))
 if alumnos >= 100:
     costo = 65
