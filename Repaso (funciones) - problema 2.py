95cb8ee6096db45a703afe9e1739e12b72152b3e
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-19 08:59:56 -0700

    Update Repaso (funciones) - problema 2.py

--- Repaso (funciones) - problema 2.py
+++ Repaso (funciones) - problema 2.py
@@ -1,5 +1,5 @@
 #Escribe una función llamada sumar(a, b) que reciba dos números y devuelva su suma. Prueba 1: sumar(8, 5): 13 Prueba 2: sumar(-3, 10): 7 Prueba 3: sumar(0, 0): 0
-
+#en la funcion se dan dos numeros que seran sumados y arrojaran un resultado
 def sumar (numero1, numero2):
     return numero1 + numero2
 
