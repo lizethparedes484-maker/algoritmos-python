455743a19ecbfd9e531f8ecbf8e65524dda94c95
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-19 08:57:26 -0700

    Update Repaso (funciones) - problema 1.py

--- Repaso (funciones) - problema 1.py
+++ Repaso (funciones) - problema 1.py
@@ -1,5 +1,5 @@
 #Crea una función llamada saludar(nombre) que reciba un nombre como cadena de texto y retorne "Hola, <nombre>!"Prueba 1: saludar("Carlos"): "Hola, Carlos!" Prueba 2: saludar("Ana"): "Hola, Ana!" Prueba 3: saludar("Mundo"): "Hola, Mundo!"
-
+#en esta funcion se ingresa el nombre y la funcion saluda a ese nombre 
 def saludar(nombre):
     return f"Hola, {nombre}!"
 
