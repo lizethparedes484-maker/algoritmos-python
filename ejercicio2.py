687963f9e9ffbc861be39bf62b01a75f83e5e55b
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-19 10:28:27 -0700

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,6 +1,12 @@
 #Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo, pero cuando se les realiza la entrevista sólo se les pregunta el año enque nacieron.
-
+# Le pedimos al usuario su año de nacimiento y lo pasamos a un número entero
 persona_edad = int(input("ingrese su año de nacimiento"))
+
+#definimos el año como punto de referencia
 año_actual= 2026
+
+# Restamos el año de nacimiento al año actual para calcular los años cumplidos
 edad = año_actual - persona_edad
+
+#mostramos el resultado
 print (f"su edad es {edad}")
