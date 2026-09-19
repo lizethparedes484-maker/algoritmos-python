91e58fb69440a33cbe8e4bf590f303506722c649
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-18 21:16:33 -0700

    Update ejercicio12.py

--- ejercicio12.py
+++ ejercicio12.py
@@ -6,7 +6,7 @@ if personas > 200 and personas <=300:
     costo = 85
     total = personas* costo
 elif personas> 300:
-    costo = 70
+    costo = 75
     total = personas  * costo
 else:
     costo = 95
