5b062d71b0c5203eaab4c3fed6746e2761af3b54
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-18 21:15:15 -0700

    Update ejercicio12.py

--- ejercicio12.py
+++ ejercicio12.py
@@ -5,7 +5,7 @@ personas = int(input("¿Cuántos personas son? "))
 if personas > 200 and personas <=300:
     costo = 85
     total = personas* costo
-elif personas>= 300:
+elif personas> 300:
     costo = 70
     total = personas  * costo
 else:
