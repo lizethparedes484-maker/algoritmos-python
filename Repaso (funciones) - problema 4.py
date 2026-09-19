f8e745d6a01b4daafd49ff1665e8f3eb3309bb02
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-19 09:11:39 -0700

    Update Repaso (funciones) - problema 4.py

--- Repaso (funciones) - problema 4.py
+++ Repaso (funciones) - problema 4.py
@@ -1,7 +1,9 @@
 #Crea una función llamada obtener_doble(n) que tome un número y devuelva su valor multiplicado por 2. Prueba 1: obtener_doble(14): 28 Prueba 2: obtener_doble(0): 0 Prueba 3: obtener_doble(-5): -10
+# Definimos una función llamada 'obtener_double' que recibe un número 
 def obtener_double (n):
+   # Toma ese número 'n', lo multiplica por 2 y devuelve el resultado 
     return n*2
-
+# Llamamos a la función con el número pedido e imprimimos el resultado 
 print(obtener_double(14))
 print(obtener_double(0))
 print(obtener_double(5))
