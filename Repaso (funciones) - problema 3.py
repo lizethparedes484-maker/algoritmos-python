88d4dbe6761ae37f6ba148cef4fe1bd56cd5bd6c
lizethparedes484-maker <lizethparedes484@gmail.com>
2026-09-19 09:04:56 -0700

    Update Repaso (funciones) - problema 3.py

--- Repaso (funciones) - problema 3.py
+++ Repaso (funciones) - problema 3.py
@@ -1,5 +1,7 @@
 #Diseña una función llamada repetir_texto(texto, veces) que devuelva la palabra repetida la cantidad de veces indicada. Prueba 1: repetir_texto("Eco", 3): "EcoEcoEco"Prueba 2: repetir_texto("Hola", 1): "Hola" Prueba 3: repetir_texto("Ja", 4): "JaJaJaJa"
+# Definimos la función que recibe un texto y el número de veces que quiero que se repita
 def repetir_texto (texto, veces):
+    # Multiplicamos el texto por 'veces' para duplicarlo ese número de ocasiones y lo devolvemos
     return texto*veces
 
 print (repetir_texto("darinel ",4))
